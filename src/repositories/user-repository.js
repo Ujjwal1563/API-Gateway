1c3ab0c449e54663d60d3deb7c8d7c486284bc1c
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-06 13:05:08 +0530

    Added new user signup api

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,10 @@
+const CrudRepository= require('./crud-repository');
+const { User } = require('../models')
+
+class UserRespository extends CrudRepository{
+    constructor (){
+        super (User);
+    }
+}
+
+module.exports = UserRespository;
