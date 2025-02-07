3a8b4eb6a08b4d1aeb29ad178379da4167b02eeb
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-07 12:07:14 +0530

    Added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-const express = require('express');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
 
 const app = express();
 app.use(express.json());
-app.use (express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
+app.use(
+  "/flightsService",
+  createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true })
+);
 
-app.use('/api', apiRoutes);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+  })
+);
+
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
